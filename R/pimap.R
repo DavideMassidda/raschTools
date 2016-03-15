fb5305243cae4b5c639af44455152e8c7bc817e7
DavideMassidda <davide.massidda@gmail.com>
2016-03-15 14:56:38 +0100

    release 0.0-0

---
+++ R/pimap.R
@@ -0,0 +1,44 @@
+pimap <- function(data.pr, data.it, logit.pr, logit.it, item, thr, rel_widths=c(1,2), logit.range=NULL, binwidth=0.25, xlab.pr="", xlab.it="Item", ylab.pr="Count", ylab.it="logit", title.pr="Person\'s abilities", title.it="Item thresholds")
+{
+    # Checks on data
+    if(!is.factor(data.it[,item]))
+        data.it[,item] <- as.factor(data.it[,item])
+    if(!is.factor(data.it[,thr]))
+        data.it[,thr] <- as.factor(data.it[,thr])
+    # Range of logits (limits of axes)
+    if(is.null(logit.range)) {
+        logit.range <- range(c(data.pr[,logit.pr], data.it[,logit.it]))
+        logit.range[1] <- .decimal_floor(logit.range[1])
+        logit.range[2] <- .decimal_ceiling(logit.range[2])
+    }
+    # Person's abilities panel
+    pr <- ggplot(data.pr, aes_string(x=logit.pr)) +
+        geom_histogram(binwidth=binwidth, colour="black", fill="gray80") +
+        scale_y_reverse() +
+        coord_flip() +
+        scale_x_continuous(limits=logit.range) +
+        xlab(xlab.pr) + ylab(ylab.pr) +
+        ggtitle(title.pr) +
+        theme_bw(base_size=18)
+    # Item thresholds panel
+    it <- ggplot(data.it, aes_string(x=item, y=logit.it)) +
+        geom_line() +
+        geom_point(shape=21, bg="gray80", size=5, show.legend=FALSE) +
+        geom_text(aes_string(label=thr),hjust=-1.5, vjust=0) +
+        scale_y_continuous(limits=logit.range) +
+        xlab(xlab.it) + ylab(ylab.it) +
+        ggtitle(title.it) +
+        theme_bw(base_size=18) +
+        theme(legend.direction="horizontal", legend.position="bottom")
+    # Merging plots
+    it <- ggplot_gtable(ggplot_build(it))
+    pr <- ggplot_gtable(ggplot_build(pr))
+    # View plots
+    plot_grid(pr, it, rel_widths=rel_widths)
+}
+# Esempio d'uso
+#set.seed(22)
+#persons <- data.frame(id=factor(1:500),logit=rnorm(500))
+#items <- data.frame(item=factor(rep(1:20,each=3)),thr=factor(rep(1:3,20)),logit=rnorm(60))
+#pi_map(persons, items, logit.pr="logit", logit.it="logit", item="item", thr="thr")
+
---
+++ R/rmfit.R
@@ -0,0 +1,33 @@
+rmfit <- function(model,person.par=NULL)
+{
+    # 'model': Un modello di classe dRm/Rm/eRm
+    fit <- list(item=NULL,person=NULL)
+    # Items
+    k <- length(model$betapar)
+    MSE2 <- sum(model$se.beta^2)/k
+    SD2 <- sum((model$betapar-mean(model$betapar))^2)/k
+    SA2 <- SD2 - MSE2
+    G <- sqrt(SA2/MSE2)
+    H <- (4*G+1)/3
+    R <- SA2/SD2
+    fit$item <- c(SA2=SA2,SD2=SD2,RMSE=sqrt(MSE2),G=G,H=H,R=R)
+    rm(k,MSE2,SD2,SA2,G,H,R)
+    # Persons
+    if(is.null(person.par))
+        person.par <- person.parameter(model)$thetapar[[1]]
+    k <- length(person.par)
+    MSE2 <- sum(person.par^2)/k
+    SD2 <- sum((person.par-mean(person.par))^2)/k
+    SA2 <- SD2 - MSE2
+    G <- sqrt(SA2/MSE2)
+    H <- (4*G+1)/3
+    R <- SA2/SD2
+    fit$person <- c(SA2=SA2,SD2=SD2,RMSE=sqrt(MSE2),G=G,H=H,R=R)
+    class(fit) <- "rmfit"
+    return(fit)
+}
+
+print.rmfit <- function(x,...)
+{
+    print(round(x,2))
+}
